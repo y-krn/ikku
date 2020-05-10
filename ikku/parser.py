0511d64620afb993822370baace31789bc4be431
ottanjp <8692074+ottanjp@users.noreply.github.com>
2020-05-10 12:32:47 +0900

    Fix: Change the package used from mecab-python3 to natto-py

--- ikku/parser.py
+++ ikku/parser.py
@@ -1,16 +1,13 @@
-import MeCab
+from natto import MeCab
 from .node import Node
 import os
 
 class Parser:
     def parse(self, text):
-        m = MeCab.Tagger()
-        m.parse("")
-        n = m.parseToNode(text)
+        m = MeCab()
         results = []
-        while n:
+        for n in m.parse(text, as_nodes=True):
             tmp = Node(n.surface, n.feature, n.stat)
             if tmp.analyzable():
                 results.append(tmp)
-            n = n.next 
         return results
