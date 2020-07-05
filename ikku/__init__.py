4c6b179bfc766184d0ab20e50f31c2b3f08213e7
ottanjp <8692074+ottanjp@users.noreply.github.com>
2020-07-05 11:43:42 +0900

    🎉Release: v0.1.3

--- ikku/__init__.py
+++ ikku/__init__.py
@@ -1,10 +1,10 @@
 from .reviewer import Reviewer
 
 __copyright__ = 'Copyright (C) 2020 Yohei Kirino'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __license__ = 'MIT'
 __author__ = 'Yohei Kirino'
 __author_email__ = 'kirino.yohei@gmail.com'
-__url__ = 'https://github.com/Gappo-Gappo/ikku'
+__url__ = 'https://github.com/ottanjp/ikku'
 
 __all__ = ['Reviewer']
